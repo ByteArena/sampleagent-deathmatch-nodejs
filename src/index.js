4ec4c57b57ed44c0382ad552ae5df7068efc234f
Jérôme Schneider / Net Gusto <contact@netgusto.com>
2017-11-29 07:52:54 +0100

    Added comments

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import {vector, comm} from 'bytearena-sdk';
-
+import { vector, comm } from "bytearena-sdk";
 const Vector2 = vector.Vector2;
+
+// Connecting our agent to the game using the BA JS SDK
 const agent = comm.connect();
 
-agent.on('perception', perception => {
+// The JS SDK exposes an event-emitter API
+// We subscribe to the perception of the world
+agent.on("perception", perception => {
   const actions = [];
 
-  const force = new Vector2(0, 1);
+  // In this scaffolding code we're blindly moving forward
+  // The game gave us a data structure containing our current perception of the world
+  // You probably want to handle this perception to react accordingly instead
 
-  actions.push({ method: 'steer', arguments: force.toArray(5) });
+  const steering = new Vector2(0, 1); // x: 0, y: 1 means "I want to move 0 meters lateraly and 1 meter forward"
+  actions.push({ method: "steer", arguments: steering.toArray() });
 
-  // Pushing batch of mutations
+  // Submitting our actions for this turn
   agent.takeActions(actions);
 });
